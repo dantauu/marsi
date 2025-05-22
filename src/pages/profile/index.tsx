5e3811bbc01f82896cea4c955fdacf208a5ebaa6
dantauu <azazzzz455u@gmail.com>
2025-05-22 16:50:39 -0700

    fix build

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,24 +1,24 @@
 import { Link } from "@tanstack/react-router"
-import { useTelegram } from "../../app/providers/telegram"
-import { useState } from "react"
+// import { useTelegram } from "../../app/providers/telegram"
+// import { useState } from "react"
 
-interface ProfileForm {
-  name: string
-  age: string
-  bio: string
-  interests: string[]
-  lookingFor: string
-}
+// interface ProfileForm {
+//   name: string
+//   age: string
+//   bio: string
+//   interests: string[]
+//   lookingFor: string
+// }
 
 const Profile = () => {
-  const { webApp, user } = useTelegram()
-  const [profile, setProfile] = useState<ProfileForm>({
-    name: user?.first_name || "",
-    age: "",
-    bio: "",
-    interests: [],
-    lookingFor: "Отношения",
-  })
+  // const { webApp, user } = useTelegram()
+  // const [profile, setProfile] = useState<ProfileForm>({
+  //   name: user?.first_name || "",
+  //   age: "",
+  //   bio: "",
+  //   interests: [],
+  //   lookingFor: "Отношения",
+  // })
 
   return (
     <div className="">
