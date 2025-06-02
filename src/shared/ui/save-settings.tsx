21a90a241c3f4a358bf2e396e5c11cabbd93fa18
dantauu <azazzzz455u@gmail.com>
2025-06-01 21:39:33 -0700

    fix filters

--- src/shared/ui/save-settings.tsx
+++ src/shared/ui/save-settings.tsx
@@ -2,7 +2,7 @@ import Button from "./button"
 
 const SaveSettingsNav = () => {
   return (
-    <div className="flex justify-between items-center h-[80px] fixed bottom-0 left-0 w-full px-[20px] rounded-tr-[28px] rounded-tl-[28px] bg-[#cecece87] ">
+    <div className="flex justify-between items-center h-[93px] pb-[12px] fixed bottom-0 left-0 w-full px-[20px] rounded-tr-[28px] rounded-tl-[28px] bg-[#cecece87] ">
       <Button
         className="w-[155px] h-[55px] font-ManropeM text-[18px]"
         variant="red"
