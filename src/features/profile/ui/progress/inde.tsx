d036de5562ac0c22b348d8ef7eae8ae16ba6bc28
dantauu <azazzzz455u@gmail.com>
2025-06-02 23:48:38 -0700

    refactor arch

--- src/features/profile/ui/progress/inde.tsx
+++ src/features/profile/ui/progress/inde.tsx
@@ -3,7 +3,7 @@ import Button from "@/shared/ui/button"
 const Progress = () => {
   return (
     <div className="flex items-center px-2 mt-[20px] h-[42px] shadow-shadow-block rounded-[10px]">
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between items-center w-full">
         <div className="flex items-center gap-2">
           <div className="">
             <p className="font-HelveticaB text-[16px] mini-mobile:text-[18px]">
