da93347c6fe2c8504fb612b6e41d03111e7e2dc5
Artem <azazzzz455u@gmail.com>
2025-06-17 01:20:52 -0700

    fix ..?

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ routesDirectory: "src/app/routes" }), tailwindcss()],
+  plugins: [react(), TanStackRouterVite({ routesDirectory: "./src/app/routes" }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
