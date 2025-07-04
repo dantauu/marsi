31221b19771e80b36e4710bf8af125c46631a5af
Artem <azazzzz455u@gmail.com>
2025-07-03 21:35:15 -0700

    fix build

--- src/features/profile-edit/ui/edit-about/index.tsx
+++ src/features/profile-edit/ui/edit-about/index.tsx
@@ -5,11 +5,11 @@ export const EditAbout = () => {
     <div className="flex flex-col justify-center gap-4 py-[6px] px-[4px] w-full shadow-shadow-block rounded-[18px]">
       <div>
         <p className="text-xl font-HelveticaB">О себе</p>
-        <ItemEdit title="Расскажите о себе" />
+        <ItemEdit onClick={() => {}} title="Расскажите о себе" />
       </div>
       <div>
         <p className="text-xl font-HelveticaB">Ваше образование</p>
-        <ItemEdit title="Кем вы работаете" />
+        <ItemEdit onClick={() => {}} title="Кем вы работаете" />
       </div>
     </div>
   )
