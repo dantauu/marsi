5f64380089dc3f82d10f5ef0f4397e9009ed88ea
Artem <azazzzz455u@gmail.com>
2025-07-10 16:52:59 -0700

    start update data

--- src/widgets/card/ui.tsx
+++ src/widgets/card/ui.tsx
@@ -1,21 +1,30 @@
 import { MockCardData } from "@/lib/data/cards"
 import { Card } from "@/entities/search/index.ts"
 import type { UserCardSearch } from "@/app/types/global.d.ts"
+import { Route } from "@/app/routes/_app/_layout/search"
+import { useGetUsersQuery } from "@/redux/api/user.ts"
+import { useSearch } from "@tanstack/react-router"
 
 const CardHuman = ({ data }: { data: UserCardSearch[] }) => {
   const avatar = new Map<number, string>(
     MockCardData.map((item) => [item.id, item.avatar])
   )
+  const params = useSearch({ from: Route.id })
+  const { data: user } = useGetUsersQuery(params)
+  if (!data) throw new Error("No user found.")
+  const userPhoto = new Map(user?.map((item) => [item.id, item.photo_url]))
   return (
     <div className="grid grid-cols-2 justify-items-center gap-y-3">
-      {data.map((item) => (
-        <Card
-          key={item.id}
-          photo_url={avatar.get(Number(item.id))}
-          age={item.age}
-          first_name={item.first_name}
-        />
-      ))}
+      {data.map((item) => {
+        const photo = userPhoto.get(item.id) ?? avatar.get(Number(item.id)) ?? ""
+          return (
+          <Card
+        key = { item.id }
+        photo_url = {photo}
+        age = { item.age }
+        first_name = { item.first_name }
+        />)
+      })}
     </div>
   )
 }
