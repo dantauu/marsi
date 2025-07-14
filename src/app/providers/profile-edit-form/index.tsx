6578ce08a79cd6f2a6fb5d60f8ff7cea9b3f67f9
Artem <azazzzz455u@gmail.com>
2025-07-13 20:21:41 -0700

    refactor way reload page with values

--- src/app/providers/profile-edit-form/index.tsx
+++ src/app/providers/profile-edit-form/index.tsx
@@ -1,10 +1,11 @@
 import { z } from "zod"
 import { useForm } from "react-hook-form"
-import { type PropsWithChildren, useCallback } from "react"
+import { type PropsWithChildren, useCallback, useEffect, useRef } from "react"
 import { EditFormContext } from "./profile-edit-context.tsx"
 import { useTelegram } from "@/app/providers/telegram"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useGetUsersQuery } from "@/redux/api/user.ts"
+import type { User } from "@/app/types/global"
 
 export const editSchema = z.object({
   photo_url: z.array(z.string()),
@@ -30,17 +31,34 @@ export const editSchema = z.object({
 
 export type EditFormSchema = z.infer<typeof editSchema>
 
-export function useFormEmptyValues(): {values: EditFormSchema | null, isLoading: boolean} {
-  const { user: telegramUser } = useTelegram()
-  const { data: users, isLoading: usersLoading } = useGetUsersQuery()
-  const user = users?.find((u) => Number(u.id) === telegramUser?.id)
-  const isLoading = usersLoading || !telegramUser
-  if (isLoading) {
-    return { values: null, isLoading: true }
+function fetchUser(user?: User | null): Partial<EditFormSchema> {
+  if (!user) return {}
+
+  return {
+    photo_url: Array.isArray(user.photo_url)
+      ? user.photo_url
+      : user.photo_url
+        ? [user.photo_url]
+        : [],
+    first_name: user.first_name ?? "",
+    age: user.age?.toString() ?? "16",
+    height: user.height?.toString() ?? "140",
+    city: user.city ?? "",
+    gender: user.gender ?? "",
+    goal: user.goal ?? "",
+    hobbies: Array.isArray(user.hobbies) ? user.hobbies : [],
   }
+}
+
+
+export function useFormEmptyValues(): {fallbackValues: EditFormSchema, values: Partial<EditFormSchema> | null, isLoaded: boolean} {
+  const { user: telegramUser } = useTelegram()
+  const { data: users, isFetching } = useGetUsersQuery()
+  const user = users?.find((u) => Number(u.id) === telegramUser?.id) ?? null
+
   const fallbackUser = {
     photo_url: [],
-    first_name: telegramUser?.first_name ?? "",
+    first_name: "",
     age: "16",
     height: "140",
     city: "",
@@ -49,35 +67,23 @@ export function useFormEmptyValues(): {values: EditFormSchema | null, isLoading:
     hobbies: [],
   }
 
-  const filledUser = user ?? fallbackUser
-
   return {
-    isLoading: false,
-    values: {
-    photo_url: Array.isArray(filledUser?.photo_url)
-      ? filledUser?.photo_url
-      : filledUser?.photo_url
-        ? [filledUser?.photo_url]
-        : [],
-    first_name: filledUser?.first_name ?? "",
-    age: filledUser?.age ?? "16",
-    height: filledUser?.height ?? "140",
-    city: filledUser?.city ?? "",
-    gender: filledUser?.gender ?? "",
-    goal: filledUser?.goal ?? "",
-    hobbies: filledUser?.hobbies ?? [],
-    },
+    values: fetchUser(user),
+    fallbackValues: fallbackUser,
+    isLoaded: !isFetching,
   }
 }
 
 type EditFormProps = PropsWithChildren<{
   defaultValues: EditFormSchema
+  values: Partial<EditFormSchema> | null
   onSubmit: (data: EditFormSchema) => void
 }>
 
 export function EditProfileProvider({
   defaultValues,
   onSubmit,
+  values,
   children,
 }: EditFormProps) {
   const form = useForm<EditFormSchema>({
@@ -85,6 +91,14 @@ export function EditProfileProvider({
     mode: "onChange",
     defaultValues,
   })
+  const hasReset = useRef(false)
+
+  useEffect(() => {
+    if (values && !hasReset.current) {
+      form.reset({ ...defaultValues, ...values })
+      hasReset.current = true
+    }
+  }, [values, defaultValues, form])
 
   const handleSubmit = useCallback(
     (data: EditFormSchema) => {
