8858c89c5693fd427c349d3493856887fd5aa4f2
Artem <azazzzz455u@gmail.com>
2025-07-22 20:45:02 -0700

    fast fix currentUser

--- src/features/profile/ui/like-count/index.tsx
+++ src/features/profile/ui/like-count/index.tsx
@@ -2,10 +2,15 @@ import SvgArrow from "@/assets/icons/Arrow"
 import Button from "@/shared/ui/buttons/button.tsx"
 import { useNavigate } from "@tanstack/react-router"
 import { useGetLikesToMeQuery } from "@/shared/api/user.ts"
+import { useCurrentUser } from "@/lib/hooks/use-current-user.ts"
 
 export const LikeCount = () => {
   const navigate = useNavigate()
-  const { data } = useGetLikesToMeQuery()
+  const { user: currentUser } = useCurrentUser()
+  if (!currentUser) throw new Error("User does not exist")
+  const { data } = useGetLikesToMeQuery(currentUser?.id, {
+    skip: !currentUser?.id
+  })
   return (
     <div className="flex flex-col items-center justify-between py-2 w-full mini-mobile:w-[210px] h-[85px] mini-mobile:h-[105px] shadow-shadow-block rounded-[10px]">
       <div className="flex items-center gap-1">
