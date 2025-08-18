224638ffa882675134a05b1d6685ccff401af4c1
Artem <azazzzz455u@gmail.com>
2025-08-17 20:11:27 -0700

    start work on save params

---
+++ src/redux/slices/filters-store.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+
+}
+
+export const filtersSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+
+  }
+})
