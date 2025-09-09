5daaa8e613a440bf2ce40f5096fa1a0aaa12cd8b
Artem <azazzzz455u@gmail.com>
2025-09-09 11:33:55 -0700

    fix query hell #2

--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useTelegram } from "@/app/providers/telegram"
 import { useInitUserMutation } from "@/shared/api/user.ts"
 
@@ -6,21 +6,26 @@ export const useInitUser = () => {
   const [initUser, { isLoading: isInitLoading, isError, isSuccess, error }] =
     useInitUserMutation()
   const { user } = useTelegram()
+  const calledRef = useRef<Set<string>>(new Set())
   const [isInit, setIsInit] = useState<boolean>(false)
 
   useEffect(() => {
+
     console.log("INIT USER TRIGGERED", user)
     if (!user) return
+    const id = String(user?.id)
+    if (calledRef.current.has(id)) return
+    calledRef.current.add(id)
 
     initUser({
-      id: String(user.id),
+      id: id,
       first_name: user.first_name,
       photo_url: user.photo_url ? [user.photo_url] : [],
       username: user.username,
     })
       .unwrap()
       .then(() => setIsInit(true))
-  }, [user, initUser])
+  }, [user])
 
   return { isInitLoading, isError, isSuccess, error, isInit }
 }
