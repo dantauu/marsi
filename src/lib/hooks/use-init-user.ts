fad0d680d6a3faf7db90e026fc67d74513703dab
Artem <azazzzz455u@gmail.com>
2025-09-10 00:53:41 -0700

    fix #5

--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -33,4 +33,5 @@ export const useInitUser = () => {
   useEffect(() => {
     console.log("data", { isLoading, isError, isSuccess, error })
   }, [isLoading, isError, isSuccess, error])
+  return {isLoading}
 }
