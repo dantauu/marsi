79073e1b8f91941b626f42be250866de785ffbd2
Artem <azazzzz455u@gmail.com>
2025-09-10 00:50:54 -0700

    fix #4

--- src/lib/hooks/use-current-user.ts
+++ src/lib/hooks/use-current-user.ts
@@ -1,14 +1,12 @@
 import { useTelegram } from "@/app/providers/telegram"
 import { useGetUserByIdQuery } from "@/shared/api/user.ts"
-import { useInitUser } from "@/lib/hooks/use-init-user.ts"
 
 export const useUserMe = () => {
   const { user: telegramUser } = useTelegram()
-  const { isSuccess } = useInitUser()
   const telegramUserId = telegramUser?.id
 
   const query = useGetUserByIdQuery(String(telegramUserId), {
-    skip: !telegramUserId || !isSuccess
+    skip: !telegramUserId
   })
 
   return {
--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -33,5 +33,4 @@ export const useInitUser = () => {
   useEffect(() => {
     console.log("data", { isLoading, isError, isSuccess, error })
   }, [isLoading, isError, isSuccess, error])
-  return {isSuccess}
 }
