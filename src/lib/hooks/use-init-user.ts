42058974f49063f887e2f2bff4555178c6e7a551
Artem <azazzzz455u@gmail.com>
2025-09-11 22:53:41 -0700

    test #3

--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -1,42 +1,39 @@
-import { useEffect, useState } from "react"
-import { useTelegram } from "@/app/providers/telegram"
-import { useAuthUserMutation, useInitUserMutation } from "@/shared/api/user.ts"
+import { useEffect, useState } from "react";
+import { useTelegram } from "@/app/providers/telegram";
+import { useAuthUserMutation, useInitUserMutation } from "@/shared/api/user.ts";
 
 export const useInitUser = () => {
-  const [initUser] = useInitUserMutation()
-  const [authUser] = useAuthUserMutation()
-  const { user, webApp } = useTelegram()
-  const [isToken, setIsToken] = useState<string | null>(localStorage.getItem("jwt"))
-  const [initialized, setInitialized] = useState(false)
+  const [initUser] = useInitUserMutation();
+  const { user } = useTelegram();
+  const [authUser] = useAuthUserMutation();
+  const [isToken, setIsToken] = useState<string | null>(localStorage.getItem("jwt"));
+  const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    if (!webApp || initialized) return
+    if (!user || isToken || initialized) return;
 
-    const interval = setInterval(async () => {
-      if (!user || isToken) return
-
+    const initialize = async () => {
       try {
         const initUserPayload = {
           id: String(user.id),
           first_name: user.first_name,
           photo_url: user.photo_url ? [user.photo_url] : [],
           username: user.username,
-        }
+        };
 
-        const initData = await initUser(initUserPayload).unwrap()
-        const { access_token } = await authUser(initData).unwrap()
+        const initData = await initUser(initUserPayload).unwrap();
+        const { access_token } = await authUser(initData).unwrap();
 
-        localStorage.setItem("jwt", access_token)
-        setIsToken(access_token)
-        setInitialized(true)
-        clearInterval(interval)
-      } catch (err) {
-        console.error(err)
+        localStorage.setItem("jwt", access_token);
+        setIsToken(access_token);
+        setInitialized(true);
+      } catch (error) {
+        console.error(error);
       }
-    }, 100)
+    };
 
-    return () => clearInterval(interval)
-  }, [webApp, user, isToken, initUser, authUser, initialized])
+    initialize();
+  }, [user, isToken, initialized, initUser, authUser]);
 
-  return { isToken }
-}
+  return { isToken };
+};
