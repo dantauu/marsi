f5a21f30d525cf7a404bf2c7c7449e9d17c0a9fd
Artem <azazzzz455u@gmail.com>
2025-09-11 22:42:47 -0700

    test #2

--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -1,37 +1,42 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { useTelegram } from "@/app/providers/telegram"
 import { useAuthUserMutation, useInitUserMutation } from "@/shared/api/user.ts"
 
 export const useInitUser = () => {
-  const [initUser] =
-    useInitUserMutation()
-  const { user } = useTelegram()
+  const [initUser] = useInitUserMutation()
   const [authUser] = useAuthUserMutation()
-  const initializedRef = useRef(false)
+  const { user, webApp } = useTelegram()
   const [isToken, setIsToken] = useState<string | null>(localStorage.getItem("jwt"))
+  const [initialized, setInitialized] = useState(false)
 
   useEffect(() => {
-    if (!user || isToken || initializedRef.current) return
-    const initialize = async () => {
+    if (!webApp || initialized) return
+
+    const interval = setInterval(async () => {
+      if (!user || isToken) return
+
       try {
-        initializedRef.current = true
         const initUserPayload = {
           id: String(user.id),
           first_name: user.first_name,
           photo_url: user.photo_url ? [user.photo_url] : [],
           username: user.username,
         }
-        const initData = await initUser(initUserPayload).unwrap()
 
+        const initData = await initUser(initUserPayload).unwrap()
         const { access_token } = await authUser(initData).unwrap()
+
         localStorage.setItem("jwt", access_token)
         setIsToken(access_token)
-      } catch (error) {
-        console.error(error)
-        initializedRef.current = false
+        setInitialized(true)
+        clearInterval(interval)
+      } catch (err) {
+        console.error(err)
       }
-    }
-    initialize()
-  }, [user, isToken])
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [webApp, user, isToken, initUser, authUser, initialized])
+
   return { isToken }
 }
