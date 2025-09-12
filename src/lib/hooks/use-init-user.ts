26b6ee9fb5d4576dec25231302cb47711305db61
Artem <azazzzz455u@gmail.com>
2025-09-11 23:13:14 -0700

    test #4

--- src/lib/hooks/use-init-user.ts
+++ src/lib/hooks/use-init-user.ts
@@ -1,39 +1,49 @@
 import { useEffect, useState } from "react";
 import { useTelegram } from "@/app/providers/telegram";
 import { useAuthUserMutation, useInitUserMutation } from "@/shared/api/user.ts";
+import { useGetUserByIdQuery } from "@/shared/api/user.ts";
 
 export const useInitUser = () => {
-  const [initUser] = useInitUserMutation();
   const { user } = useTelegram();
+  const [initUser] = useInitUserMutation();
   const [authUser] = useAuthUserMutation();
   const [isToken, setIsToken] = useState<string | null>(localStorage.getItem("jwt"));
-  const [initialized, setInitialized] = useState(false);
+
+  const { isError } = useGetUserByIdQuery(String(user?.id), {
+    skip: !user?.id || !isToken,
+  });
 
   useEffect(() => {
-    if (!user || isToken || initialized) return;
+    if (!user) return;
 
     const initialize = async () => {
       try {
-        const initUserPayload = {
-          id: String(user.id),
-          first_name: user.first_name,
-          photo_url: user.photo_url ? [user.photo_url] : [],
-          username: user.username,
-        };
+        if (isToken && isError) {
+          localStorage.removeItem("jwt");
+          setIsToken(null);
+        }
 
-        const initData = await initUser(initUserPayload).unwrap();
-        const { access_token } = await authUser(initData).unwrap();
+        if (!isToken) {
+          const initUserPayload = {
+            id: String(user.id),
+            first_name: user.first_name,
+            photo_url: user.photo_url ? [user.photo_url] : [],
+            username: user.username,
+          };
 
-        localStorage.setItem("jwt", access_token);
-        setIsToken(access_token);
-        setInitialized(true);
+          const initData = await initUser(initUserPayload).unwrap();
+          const { access_token } = await authUser(initData).unwrap();
+
+          localStorage.setItem("jwt", access_token);
+          setIsToken(access_token);
+        }
       } catch (error) {
         console.error(error);
       }
     };
 
     initialize();
-  }, [user, isToken, initialized, initUser, authUser]);
+  }, [user, isToken, isError, initUser, authUser]);
 
   return { isToken };
 };
