868c8ef9e287b285c724332414040a10011173ce
Artem <azazzzz455u@gmail.com>
2025-09-16 01:33:53 -0700

    test

--- src/redux/slices/users.ts
+++ src/redux/slices/users.ts
@@ -3,10 +3,12 @@ import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 
 type UsersState = {
   users: User[]
+  interactedIds: string[]
 }
 
 const initialState: UsersState = {
-  users: []
+  users: [],
+  interactedIds: []
 }
 
 const userSlice = createSlice({
@@ -14,13 +16,18 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     appendUsers: (state, action: PayloadAction<User[]>) => {
-      state.users.push(...action.payload)
+      const filtered = action.payload.filter((u) => !state.interactedIds.includes(u.id))
+      state.users.push(...filtered)
     },
     removedUser: (state, action: PayloadAction<string>) => {
+      if (!state.interactedIds.includes(action.payload)) {
+        state.interactedIds.push(action.payload)
+      }
       state.users = state.users.filter((u) => u.id !== action.payload)
     },
     resetUsers: (state) => {
       state.users = []
+      state.interactedIds = []
     }
   }
 })
