e5319531a13792b3804be1549b097964c7eecd92
Artem <azazzzz455u@gmail.com>
2025-09-20 19:26:59 -0700

    prettier

--- src/widgets/overlay/index.tsx
+++ src/widgets/overlay/index.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils/cn.tsx"
 
 export const Overlay = ({ className }: { className?: string }) => {
-  return <div className={cn("fixed top-0 w-full h-full z-10 bg-[#00000087]", className)}></div>
+  return (
+    <div
+      className={cn("fixed top-0 w-full h-full z-10 bg-[#00000087]", className)}
+    ></div>
+  )
 }
