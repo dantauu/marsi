00cc3fca9775f8cdc7cb9bf660b87d18f4af366b
Artem <azazzzz455u@gmail.com>
2025-09-20 22:34:47 -0700

    refactor: hook usePlatform

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -5,20 +5,18 @@ import { FilterForm } from "@/shared/lib/filter-form"
 import { useInitUser } from "@/lib/hooks/use-init-user.ts"
 import { useUserMe } from "@/lib/hooks/use-current-user.ts"
 import { useLikesSocket } from "@/lib/hooks/use-likes-socket.ts"
-import { useTelegram } from "@/app/providers/telegram"
 import { Toaster } from "sonner"
+import { usePlatform } from "@/shared/lib/hooks/use-platform.ts"
 
 const Layout = ({ children }: PropsWithChildren) => {
   useInitUser()
   const router = useRouterState()
   const { user: userMe } = useUserMe()
   useLikesSocket(userMe?.id)
-  const { webApp } = useTelegram()
-  const platform = webApp?.platform ?? ""
-  const mobile = ["android", "ios"]
+  const { isMobile } = usePlatform()
   return (
     <div
-      className={`max-w-[610px] mx-auto ${mobile.includes(platform) ? "pt-[80px]" : "pt-[20px]"}`}
+      className={`max-w-[610px] mx-auto ${isMobile ? "pt-[80px]" : "pt-[20px]"}`}
     >
       {children}
       <NavBar activePath={router.location.pathname} />
