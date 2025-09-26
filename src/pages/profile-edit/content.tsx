24daffc4b05a621dde74b1af5da7a197c17ee2c4
Artem <azazzzz455u@gmail.com>
2025-09-25 18:04:48 -0700

    fix: padding

--- src/pages/profile-edit/content.tsx
+++ src/pages/profile-edit/content.tsx
@@ -19,10 +19,10 @@ export const EditProfileContent = () => {
       {isEditOpen && <Overlay className="max-w-[610px]" />}
       <div
         data-testid="profile-edit"
-        className={`pb-[130px] ${isMobile ? "pt-[120px]" : "pt-[60px]"}`}
+        className={`pb-[130px] ${isMobile ? "pt-[50px]" : "pt-[50px]"}`}
       >
         <div
-          className={`fixed pb-[4.5px] z-5 bg-white w-full max-w-[610px] top-0 flex items-center  justify-between px-2 ${isMobile ? "pt-[85px]" : "pt-[20px]"}`}
+          className={`fixed pb-[4.5px] z-5 bg-white w-full max-w-[610px] top-0 flex items-center  justify-between px-2 ${isMobile ? "pt-[90px]" : "pt-[30px]"}`}
         >
           <SvgArrowPath
             className="w-[14px] h-[24px]"
