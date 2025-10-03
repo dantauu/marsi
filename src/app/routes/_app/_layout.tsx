1e3a374aa04fc5433d4af04ec52aeb07975250cc
Artem <azazzzz455u@gmail.com>
2025-10-03 16:30:06 -0700

    test: fix flash, add animation

--- src/app/routes/_app/_layout.tsx
+++ src/app/routes/_app/_layout.tsx
@@ -1,14 +1,28 @@
 import Layout from "@/app/layout"
-import { createFileRoute, Outlet } from "@tanstack/react-router"
+import { createFileRoute, Outlet, useRouterState } from "@tanstack/react-router"
+import { AnimatePresence, motion } from "framer-motion"
 
 export const Route = createFileRoute("/_app/_layout")({
   component: () => <LayoutContent />,
 })
 
 function LayoutContent() {
+  const location = useRouterState({ select: (s) => s.location })
   return (
     <Layout>
-      <Outlet />
+      <div className="relative overflow-hidden">
+        <AnimatePresence mode={"wait"}>
+          <motion.div
+            key={location.pathname}
+            initial={false}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 1.05 }}
+            transition={{ duration: 0.23, ease: "easeOut" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
+      </div>
     </Layout>
   )
 }
