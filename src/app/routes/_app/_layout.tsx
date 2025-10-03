10b7724bb19b3f7f2ac2bd76be6609b9ff8b0c55
Artem <azazzzz455u@gmail.com>
2025-10-03 16:53:47 -0700

    revert

--- src/app/routes/_app/_layout.tsx
+++ src/app/routes/_app/_layout.tsx
@@ -1,28 +1,14 @@
 import Layout from "@/app/layout"
-import { createFileRoute, Outlet, useRouterState } from "@tanstack/react-router"
-import { AnimatePresence, motion } from "framer-motion"
+import { createFileRoute, Outlet } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/_app/_layout")({
   component: () => <LayoutContent />,
 })
 
 function LayoutContent() {
-  const location = useRouterState({ select: (s) => s.location })
   return (
     <Layout>
-      <div className="relative overflow-hidden">
-        <AnimatePresence mode={"wait"}>
-          <motion.div
-            key={location.pathname}
-            initial={false}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 1.05 }}
-            transition={{ duration: 0.23, ease: "easeOut" }}
-          >
-            <Outlet />
-          </motion.div>
-        </AnimatePresence>
-      </div>
+      <Outlet />
     </Layout>
   )
 }
