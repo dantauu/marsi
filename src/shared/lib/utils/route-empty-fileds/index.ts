4a2ebe777081f09bddae381fbb8aa3872613e951
Artem <azazzzz455u@gmail.com>
2025-10-10 13:10:17 -0700

    fast fix

--- src/shared/lib/utils/route-empty-fileds/index.ts
+++ src/shared/lib/utils/route-empty-fileds/index.ts
@@ -2,14 +2,16 @@ import { useNavigate } from "@tanstack/react-router"
 import { GetFields } from "@/lib/utils/get-fields"
 import { useEffect } from "react"
 import { getEnvironment } from "@/shared/lib/utils/get-environment"
+import { useUserMe } from "@/shared/lib/hooks/use-user-me.ts"
 
 const useRouteEmptyFields = () => {
   const { isDev } = getEnvironment()
   const { isEmpty } = GetFields()
+  const { user } = useUserMe()
   const navigate = useNavigate()
   useEffect(() => {
-    if (isEmpty && !isDev) navigate({ to: "/fill-fields" })
-  }, [isEmpty, navigate, isDev])
+    if (isEmpty && !isDev && user?.id) navigate({ to: "/fill-fields" })
+  }, [isEmpty, navigate, isDev, user?.id])
 }
 
 export default useRouteEmptyFields
