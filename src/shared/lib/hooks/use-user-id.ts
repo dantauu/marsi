ea824b1770b94fc5f241f91f4e0e6e201c5e1e0b
Artem <azazzzz455u@gmail.com>
2025-10-19 20:51:00 -0700

    show data in token

--- src/shared/lib/hooks/use-user-id.ts
+++ src/shared/lib/hooks/use-user-id.ts
@@ -3,7 +3,9 @@ import { parseToken } from "@/lib/utils/jwt"
 
 export const useUserId = () => {
   const token = useAppSelector((state) => state.auth.token)
+  console.log("token:", token)
   const payload = parseToken(token)
+  console.log("payload:", payload)
 
   return payload?.sub ?? null
 }
