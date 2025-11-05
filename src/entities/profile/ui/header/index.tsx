74a79b6fc082b521ba69183e257f41a2de952707
Artem <azazzzz455u@gmail.com>
2025-11-05 12:32:50 +0300

    dark theme - 80%

--- src/entities/profile/ui/header/index.tsx
+++ src/entities/profile/ui/header/index.tsx
@@ -32,7 +32,7 @@ export const ProfileHeader = ({ currentUser }: ProfileHeaderProps) => {
         </Button>
         <div className="flex items-center gap-2">
           <div className="flex items-cente">
-            <p className="font-ManropeEB text-[16px] mini-mobile:text-[19px] text-ellipsis overflow-hidden max-w-[115px] whitespace-nowrap text-[var(--color-text-white)]">
+            <p className="font-ManropeEB text-[16px] mini-mobile:text-[19px] text-ellipsis overflow-hidden max-w-[115px] whitespace-nowrap text-[var(--color-text-black)]">
               {currentUser?.first_name || "Не указано"}
             </p>
             <p className="font-ManropeEB text-[16px] mini-mobile:text-[19px] text-[var(--color-text-white)]">
