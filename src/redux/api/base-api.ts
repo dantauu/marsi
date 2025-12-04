b177c94a2e6315e4dc2a484fff9f2c7e16745ecd
dantauu <azazzzz455u@gmail.com>
2025-12-04 00:47:22 -0800

    test draggable photos

--- src/redux/api/base-api.ts
+++ src/redux/api/base-api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000/",
+    baseUrl: import.meta.env.VITE_BASE_URL || "http://localhost:9000/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("jwt")
       if (token) headers.set("Authorization", `Bearer ${token}`)
