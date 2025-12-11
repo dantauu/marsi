5affb59bf674301b70667d8df152a061c35867e9
dantauu <azazzzz455u@gmail.com>
2025-12-11 15:58:14 -0800

    close app

--- src/pages/deleted/index.tsx
+++ src/pages/deleted/index.tsx
@@ -1,13 +1,15 @@
 import Button from "@/shared/ui/buttons/button.tsx"
+import { useTelegram } from "@/app/providers/telegram"
 
 export const DeletedPage = () => {
+  const { closeApp } = useTelegram()
   return (
     <div className="flex flex-col gap-5 justify-center items-center min-h-screen">
       <div className="text-[var(--color-text-black)]">
         <p>Аккаунт удален, жаль что Вы уходите</p>
         <p>Но в любой момент вы можете вернуться :)</p>
       </div>
-      <Button className="text-[var(--color-text-black)]" variant={"default"}>
+      <Button onClick={closeApp} className="text-[var(--color-text-black)]" variant={"default"}>
         Закрыть
       </Button>
     </div>
