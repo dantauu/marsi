4a85cdfef09ab1f93f8eb082af2017b1e015de53
dantauu <azazzzz455u@gmail.com>
2026-02-06 19:48:42 -0800

    fixup base-api

--- src/redux/api/base-api.ts
+++ src/redux/api/base-api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000/", //import.meta.env.VITE_BASE_URL
+    baseUrl: import.meta.env.VITE_BASE_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("jwt")
       if (token) headers.set("Authorization", `Bearer ${token}`)
